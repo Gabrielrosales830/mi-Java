863f18ddd1ff795eb93233757155af0fa026c1bf
Gabriel Rosales <gabriel.rosales903@std.uni.edu.ni>
2025-06-25 16:56:11 -0600

    Agrega multiplicacion num1 y num2

--- Holamundo.java
+++ Holamundo.java
@@ -4,7 +4,9 @@ public static void main(String[] args) {
     int num1= 10;
     int num2 = 1 ;
     int suma = num1 + num2;
+    int multiplicacion= num1*num2;
     System.out.println("Hola"+nombre+"!!");
     System.out.println("La suma de "+num1+ "+" +num2+ "es :" + suma );
+    System.out.println("La multiplicacion "+num1+"+"+ num2+ "es :"+ multiplicacion);
 }
 }
