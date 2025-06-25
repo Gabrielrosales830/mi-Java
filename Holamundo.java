daa43cde7e3299d48bf852c9125c1cdc7ccee31a
Gabriel Rosales <gabriel.rosales903@std.uni.edu.ni>
2025-06-25 16:27:36 -0600

    Mi primer cambio confirmado

---
+++ Holamundo.java
@@ -0,0 +1,10 @@
+public class Holamundo {
+public static void main(String[] args) {
+    String nombre = "Estudiantes ";
+    int num1= 5;
+    int num2 = 3 ;
+    int suma = num1 + num2;
+    System.out.println("Hola"+nombre+"!!");
+    System.out.println("La suma de "+num1+ "+" +num2+ "es :" + suma );
+}
+}
